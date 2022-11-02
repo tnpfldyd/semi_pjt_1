18baf0279df6bb806a1e63ef15e4f8a721790d75
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-02 14:42:59 +0900

    Update/chats

--- chats/forms.py
+++ chats/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from .models import DirectMessage
-
+from django.forms import TextInput
 
 class DirectMessageForm(forms.ModelForm):
     class Meta:
         model = DirectMessage
         fields = ('content',)
+        widgets = {
+            'content': TextInput(attrs={
+                'placeholder': '보낼 메세지를 입력하세요',
+            })
+        }
+        labels = {
+            'content': ''
+        }
--- chats/views.py
+++ chats/views.py
@@ -4,15 +4,8 @@ from django.contrib.auth.decorators import login_required
 from .forms import DirectMessageForm
 from django.contrib.auth import get_user_model
 from django.views.decorators.http import require_POST
-
+from django.http import JsonResponse
 # Create your views here.
-
-def test(request):
-    form = DirectMessageForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('chats:index')
-    return render(request, 'chats/test.html', {'form':form})
 
 @login_required
 def index(request):
@@ -30,8 +23,10 @@ def detail(request, room_pk):
         user = get_user_model().objects.get(pk=request.user.pk)
         send = user.send_user.all()
         receiver = user.receiver_user.all()
+        if request.user.id != room_info.last_user_id:
+            room_info.count = 0
+        room_info.save()
         messages = DirectMessage.objects.filter(room_number_id=room_pk)
-        room_info = get_object_or_404(MessageRoom, pk=room_pk)
         form = DirectMessageForm()
         context = {
             "room_info": room_info,
@@ -51,38 +46,64 @@ def send(request, pk):
             room = MessageRoom.objects.get(to_user_id=request.user.id, from_user_id=pk)
             temp = form.save(commit=False)
             temp.room_number_id = room.id
-            temp.who_id = pk
+            temp.recipient_id = pk
             temp.save()
-            room.last_user = request.user.username
+            room.last_user_id = request.user.id
             room.last_message = temp.content
-            room.count += 1
+            if request.user.id != temp.recipient_id:
+                room.count += 1
             room.save()
-            return redirect("chats:detail", room.pk)
+            arr = []
+            messages = DirectMessage.objects.filter(room_number_id=room.pk)
+            for i in messages:
+                arr.append((i.recipient.pk, i.content, i.create_at))
+            context = {
+                'content': arr,
+            }
+            return JsonResponse(context)
         else:
             if MessageRoom.objects.filter(to_user_id=pk, from_user_id=request.user.id).exists():
                 room = MessageRoom.objects.get(to_user_id=pk, from_user_id=request.user.id)
                 temp = form.save(commit=False)
                 temp.room_number_id = room.id
-                temp.who_id = pk
+                temp.recipient_id = pk
                 temp.save()
-                room.last_user = request.user.username
+                room.last_user_id = request.user.id
                 room.last_message = temp.content
-                room.count += 1
+                if request.user.id != temp.recipient_id:
+                    room.count += 1
                 room.save()
-                return redirect("chats:detail", room.pk)
+                arr = []
+                messages = DirectMessage.objects.filter(room_number_id=room.pk)
+                for i in messages:
+                    arr.append((i.recipient.pk, i.content, i.create_at))
+                context = {
+                    'content': arr,
+                }
+                return JsonResponse(context)
             else:
                 temp = form.save(commit=False)
                 room = MessageRoom.objects.create(
                     to_user_id=request.user.id,
                     from_user_id=pk,
-                    count=1,
-                    last_user=request.user,
+                    count = 0,
+                    last_user_id=request.user.id,
                     last_message=temp.content,
                 )
                 temp.room_number_id = room.id
-                temp.who_id = pk
+                temp.recipient_id = pk
+                if request.user.id != temp.recipient_id:
+                    room.count = 1
+                room.save()
                 temp.save()
-                return redirect("chats:detail", room.pk)
+                arr = []
+                messages = DirectMessage.objects.filter(room_number_id=room.pk)
+                for i in messages:
+                    arr.append((i.recipient.pk, i.content, i.create_at))
+                context = {
+                    'content': arr,
+                }
+                return JsonResponse(context)
     return render(request, "chats/detail.html", {"form": form})
 
 
@@ -94,11 +115,12 @@ def first_send(request, pk):
             room = MessageRoom.objects.get(to_user_id=request.user.id, from_user_id=pk)
             temp = form.save(commit=False)
             temp.room_number_id = room.id
-            temp.who_id = pk
+            temp.recipient_id = pk
             temp.save()
-            room.last_user = request.user.username
+            room.last_user_id = request.user.id
             room.last_message = temp.content
-            room.count += 1
+            if request.user.id != temp.recipient_id:
+                room.count += 1
             room.save()
             return redirect("chats:detail", room.pk)
         else:
@@ -106,11 +128,12 @@ def first_send(request, pk):
                 room = MessageRoom.objects.get(to_user_id=pk, from_user_id=request.user.id)
                 temp = form.save(commit=False)
                 temp.room_number_id = room.id
-                temp.who_id = pk
+                temp.recipient_id = pk
                 temp.save()
-                room.last_user = request.user.username
+                room.last_user_id = request.user.id
                 room.last_message = temp.content
-                room.count += 1
+                if request.user.id != temp.recipient_id:
+                    room.count += 1
                 room.save()
                 return redirect("chats:detail", room.pk)
             else:
@@ -118,12 +141,15 @@ def first_send(request, pk):
                 room = MessageRoom.objects.create(
                     to_user_id=request.user.id,
                     from_user_id=pk,
-                    count=1,
-                    last_user=request.user,
+                    count=0,
+                    last_user_id=request.user.id,
                     last_message=temp.content,
                 )
                 temp.room_number_id = room.id
-                temp.who_id = pk
+                temp.recipient_id = pk
+                if request.user != temp.recipient_id:
+                    room.count = 1
+                room.save()
                 temp.save()
                 return redirect("chats:detail", room.pk)
     return render(request, "chats/send.html", {"form": form})
