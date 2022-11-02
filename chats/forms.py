2b5a16fe0f079f27ba76d846da5f0e9c9486fb00
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-02 09:51:55 +0900

    update

--- chats/forms.py
+++ chats/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from emoji_picker.widgets import EmojiPickerTextInputAdmin, EmojiPickerTextareaAdmin
 from .models import DirectMessage
 
 
@@ -7,6 +6,3 @@ class DirectMessageForm(forms.ModelForm):
     class Meta:
         model = DirectMessage
         fields = ('content',)
-        widgets = {
-            'content': EmojiPickerTextareaAdmin,
-        }
