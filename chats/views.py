74c3c8b0347ae49bf3fa3f9591694d8b1cd135b0
tnpfldyd <dkdldhs0322@gmail.com>
2022-11-03 16:57:46 +0900

    Updata/chats

--- chats/views.py
+++ chats/views.py
@@ -27,6 +27,11 @@ def detail(request, room_pk):
             room_info.count = 0
         room_info.save()
         messages = DirectMessage.objects.filter(room_number_id=room_pk)
+        if DirectMessage.objects.filter(room_number_id=room_pk, recipient_id=request.user, read=False).exists():
+            temp = DirectMessage.objects.filter(room_number_id=room_pk, recipient_id=request.user, read=False)
+            for i in temp:
+                i.read = True
+                i.save()
         form = DirectMessageForm()
         context = {
             "room_info": room_info,
@@ -41,7 +46,6 @@ def detail(request, room_pk):
 @require_POST
 def send(request, pk):
     form = DirectMessageForm(request.POST)
-    print(form)
     if form.is_valid():
         if MessageRoom.objects.filter(to_user_id=request.user.id, from_user_id=pk).exists():
             room = MessageRoom.objects.get(to_user_id=request.user.id, from_user_id=pk)
@@ -54,13 +58,31 @@ def send(request, pk):
             if request.user.id != temp.recipient_id:
                 room.count += 1
             room.save()
+            roominfo = []
+            user = get_user_model().objects.get(pk=request.user.pk)
+            send = user.send_user.all()
+            receiver = user.receiver_user.all()
+            sere = send.union(receiver, all=False).order_by("-updated_at")
+            for i in sere:
+                if request.user == i.to_user:
+                    temp = i.from_user.username
+                else:
+                    temp = i.to_user.username
+                if '/static/imo' in i.last_message:
+                    text = '이모티콘을 전송하였습니다.'
+                else:
+                    text = i.last_message
+                roominfo.append((i.updated_at, temp, text, i.pk))
             arr = []
             messages = DirectMessage.objects.filter(room_number_id=room.pk)
             for i in messages:
-                arr.append((i.recipient.pk, i.content, i.create_at))
+                arr.append((i.recipient.pk, i.content, i.create_at, i.read))
             context = {
                 'content': arr,
+                'room': roominfo,
             }
+
+
             return JsonResponse(context)
         else:
             if MessageRoom.objects.filter(to_user_id=pk, from_user_id=request.user.id).exists():
@@ -74,12 +96,28 @@ def send(request, pk):
                 if request.user.id != temp.recipient_id:
                     room.count += 1
                 room.save()
+                roominfo = []
+                user = get_user_model().objects.get(pk=request.user.pk)
+                send = user.send_user.all()
+                receiver = user.receiver_user.all()
+                sere = send.union(receiver, all=False).order_by("-updated_at")
+                for i in sere:
+                    if request.user == i.to_user:
+                        temp = i.from_user.username
+                    else:
+                        temp = i.to_user.username
+                    if '/static/imo' in i.last_message:
+                        text = '이모티콘을 전송하였습니다.'
+                    else:
+                        text = i.last_message
+                    roominfo.append((i.updated_at, temp, text, i.pk))
                 arr = []
                 messages = DirectMessage.objects.filter(room_number_id=room.pk)
                 for i in messages:
-                    arr.append((i.recipient.pk, i.content, i.create_at))
+                    arr.append((i.recipient.pk, i.content, i.create_at, i.read))
                 context = {
                     'content': arr,
+                    'room': roominfo,
                 }
                 return JsonResponse(context)
             else:
@@ -97,12 +135,28 @@ def send(request, pk):
                     room.count = 1
                 room.save()
                 temp.save()
+                roominfo = []
+                user = get_user_model().objects.get(pk=request.user.pk)
+                send = user.send_user.all()
+                receiver = user.receiver_user.all()
+                sere = send.union(receiver, all=False).order_by("-updated_at")
+                for i in sere:
+                    if request.user == i.to_user:
+                        temp = i.from_user.username
+                    else:
+                        temp = i.to_user.username
+                    if '/static/imo' in i.last_message:
+                        text = '이모티콘을 전송하였습니다.'
+                    else:
+                        text = i.last_message
+                    roominfo.append((i.updated_at, temp, text, i.pk))
                 arr = []
                 messages = DirectMessage.objects.filter(room_number_id=room.pk)
                 for i in messages:
-                    arr.append((i.recipient.pk, i.content, i.create_at))
+                    arr.append((i.recipient.pk, i.content, i.create_at, i.read))
                 context = {
                     'content': arr,
+                    'room': roominfo,
                 }
                 return JsonResponse(context)
     return render(request, "chats/detail.html", {"form": form})
